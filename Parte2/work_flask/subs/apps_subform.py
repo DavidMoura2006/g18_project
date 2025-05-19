cd7068c2b44bc0c42237fbb31855fdd6e4ca2b60
Jose-Rodrigues547 <up202405710@up.pt>
2025-05-19 18:51:10 +0100

    Update apps_subform.py

--- Parte2/work_flask/subs/apps_subform.py
+++ Parte2/work_flask/subs/apps_subform.py
@@ -3,7 +3,7 @@ from datafile import filename
 from classes.factory import Factory
 from classes.inventory import Inventory
 from classes.machine import Machine
-from classes.maintenancelog import Maintenancelog
+from classes.maintenancelog import MaintenanceLog
 from classes.userlogin import Userlogin
 
 prev_option = ""
