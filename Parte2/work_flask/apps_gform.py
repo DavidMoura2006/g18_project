c3e57a88db76602e3034fe14dfa618ca1051ba5e
Jose-Rodrigues547 <up202405710@up.pt>
2025-05-19 18:24:33 +0100

    Update apps_gform.py

--- Parte2/work_flask/apps_gform.py
+++ Parte2/work_flask/apps_gform.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, session
-from classes.courses import Courses
-from classes.course_instructors import Courses_Instructors
-from classes.instructor import Instructors
-from classes.platform import Platforms
+from classes.factory import factory
+from classes.inventory import inventory
+from classes.machine import machine
+from classes.maintenancelog import maintenancelog
 from classes.userlogin import Userlogin
 
 prev_option = ""
