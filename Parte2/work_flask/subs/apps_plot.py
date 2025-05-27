d9b9af4f475b7ee1cefec2a5e211717ad36676c6
guilhermefeup <up202405473@up.pt>
2025-05-27 18:47:27 +0100

    Update apps_plot.py

--- Parte2/work_flask/subs/apps_plot.py
+++ Parte2/work_flask/subs/apps_plot.py
@@ -12,19 +12,24 @@ import base64
 def apps_plot():
     engine = create_engine(f'sqlite:///{filename}factorys.db')
     df_products = pd.read_sql_table('Machine', engine)  #alterado
-    result = df_products.groupby('machines_id')['capacity'].sum() #alterado
+    result = df_products.groupby('machines_type')['capacity'].sum() #alterado
+    print(result.head())
     p_ids = result.index
     p_names = []
     for p_id in p_ids:
-        p_obj = Machine.get_by_id[p_id]
-        p_names.append(p_obj.name)
+        p_obj = Machine.obj.get(p_id)
+        if p_obj:
+            p_names.append(p_obj.machines_type)
+        else:
+            p_names.append(str(p_id))  
+
     quantities = result.values
 
     fig, ax = plt.subplots()
-    plt.bar(p_names, quantities, width=0.4, label='machines_id')  #alterado
+    plt.bar(p_names, quantities, width=0.4, label='machines_type')  #alterado
     x_index = range(len(p_names))
     plt.xticks(x_index, labels=p_names)
-    plt.xlabel('machines_id')  #alterado
+    plt.xlabel('machines_type')  #alterado
     plt.ylabel('Capacity')  #alterado
     plt.title('Machines Capacities')  #alterado
     buf = io.BytesIO()
@@ -33,9 +38,6 @@ def apps_plot():
     image = base64.b64encode(buf.getvalue()).decode('utf-8')
 
     return render_template("plot.html", image=image, ulogin=session.get("user"))
-
-
-
 
 
 
